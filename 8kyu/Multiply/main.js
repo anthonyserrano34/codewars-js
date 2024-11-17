005a631d32be9188e112aa59165e260d3d7fcce8
Anthony SERRANO <34anthonyserrano@gmail.com>
2024-11-17 09:48:17 +0100

    8kyu - Multiply - https://www.codewars.com/kata/50654ddff44f800200000004/

---
+++ 8kyu/Multiply/main.js
@@ -0,0 +1,11 @@
+function multiply(a, b) {
+	return a * b;
+}
+
+console.log(multiply(1, 1)); // Expected: 1
+console.log(multiply(2, 1)); // Expected: 2
+console.log(multiply(2, 2)); // Expected: 4
+console.log(multiply(3, 5)); // Expected: 15
+console.log(multiply(5, 0)); // Expected: 0
+console.log(multiply(0, 5)); // Expected: 0
+console.log(multiply(0, 0)); // Expected: 0
